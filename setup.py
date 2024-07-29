07b01c8798dc3436e803d6b52b4d2f01e49f8f27
Peter Gabbay <petergabbay@gmail.com>
2024-07-29 00:47:08 -0700

    Remove module

--- setup.py
+++ setup.py
@@ -3,10 +3,8 @@
 import codecs
 import os
 import re
-import sys
 
 from setuptools import find_packages, setup
-from setuptools.command.test import test as TestCommand
 
 long_description = open("README.rst").read()
 here = os.path.abspath(os.path.dirname(__file__))
@@ -28,32 +26,11 @@ def find_version(*file_paths):
     raise RuntimeError("Unable to find version string.")
 
 
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-
-        errno = pytest.main(self.test_args)
-        sys.exit(errno)
-
-
 install_requires = [
     "PyYAML",
     "wrapt",
     "yarl",
-    # Support for urllib3 >=2 needs CPython >=3.10
-    # so we need to block urllib3 >=2 for Python <3.10 and PyPy for now.
-    # Note that vcrpy would work fine without any urllib3 around,
-    # so this block and the dependency can be dropped at some point
-    # in the future. For more Details:
-    # https://github.com/kevin1024/vcrpy/pull/699#issuecomment-1551439663
     "urllib3 <2; python_version <'3.10'",
-    # https://github.com/kevin1024/vcrpy/pull/775#issuecomment-1847849962
     "urllib3 <2; platform_python_implementation =='PyPy'",
 ]
 
@@ -71,12 +48,6 @@ extras_require = {
         "requests>=2.22.0",
         "tornado",
         "urllib3",
-        # Needed to un-break httpbin 0.7.0. For httpbin >=0.7.1 and after,
-        # this pin and the dependency itself can be removed, provided
-        # that the related bug in httpbin has been fixed:
-        # https://github.com/kevin1024/vcrpy/issues/645#issuecomment-1562489489
-        # https://github.com/postmanlabs/httpbin/issues/673
-        # https://github.com/postmanlabs/httpbin/pull/674
         "Werkzeug==2.0.3",
     ],
 }
@@ -84,7 +55,7 @@ extras_require = {
 setup(
     name="vcrpy",
     version=find_version("vcr", "__init__.py"),
-    description=("Automatically mock your HTTP interactions to simplify and speed up testing"),
+    description="Automatically mock your HTTP interactions to simplify and speed up testing",
     long_description=long_description,
     long_description_content_type="text/x-rst",
     author="Kevin McCarthy",
@@ -95,7 +66,6 @@ setup(
     install_requires=install_requires,
     license="MIT",
     extras_require=extras_require,
-    tests_require=extras_require["tests"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
